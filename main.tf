cda1ebff67c33f00dccf0128c9dd635ddbf47937
PrasannaJadhav7396 <161753584+PrasannaJadhav7396@users.noreply.github.com>
2024-03-03 20:46:30 +0530

    Update Bucket Config

--- main.tf
+++ main.tf
@@ -112,7 +112,15 @@ resource "aws_s3_bucket" "shared_bucket" {
   versioning {
     enabled = true
   }
-  
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
   tags = {
     Name = "${var.environment_id} Shared Bucket"
   }
